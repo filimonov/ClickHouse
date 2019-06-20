a0a7856fd62bfa522a5cffb36e33d3cd05a5bc14
Mikhail Filimonov <mfilimonov@altinity.com>
2019-06-20 14:26:50 +0200

    Exception on bad regexp, more tests, clang-format

--- dbms/src/Common/SensitiveDataMasker.h
+++ dbms/src/Common/SensitiveDataMasker.h
@@ -1,84 +1,101 @@
 
+#include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 #include <re2/re2.h>
 #include <re2/stringpiece.h>
-#include <memory>
-#include <iostream>
+#include <Common/Exception.h>
 
 
 //* TODO: option with hyperscan? https://software.intel.com/en-us/articles/why-and-how-to-replace-pcre-with-hyperscan
 // re2::set should also work quite fast, but it doesn't return the match position, only which regexp was matched
 
-namespace DB {
+namespace DB
+{
+namespace ErrorCodes
+{
+    extern const int CANNOT_COMPILE_REGEXP;
+}
 
-    class SensitiveDataMasker
+
+class SensitiveDataMasker
+{
+private:
+    class MaskingRule
     {
-
     private:
+        const std::string name;
+        const std::string replacement_string;
+        const std::string regexp_string;
 
-        class MaskingRule
-        {
-        private:
-            const std::string name;
-            const std::string replacement_string;
-            const std::string regexp_string;
+        const RE2 regexp;
+        const re2::StringPiece replacement;
 
-            const RE2 regexp;
-            const re2::StringPiece replacement;
+        uint64_t matches_count = 0;
+        std::mutex mutex;
 
-            uint64_t matches_count = 0;
-            std::mutex mutex;
-
-            //options
-        public:
-            MaskingRule(const std::string &name, const std::string &regexpString,
-                        const std::string &replacementString) : name(name),
-                                                                replacement_string(replacementString),
-                                                                regexp_string(regexpString),
-                                                                regexp(regexp_string, RE2::Quiet),
-                                                                replacement(replacement_string) {}
-            int apply(std::string &data)
-            {
-                auto m = RE2::GlobalReplace(&data, regexp, replacement);
-                std::lock_guard lock(mutex);
-                matches_count += m;
-                return m;
-            }
-
-            const std::string &getName() const {  return name;    }
-            const std::string &getReplacementString() const {  return replacement_string;  }
-            uint64_t getMatchesCount() const   {  return matches_count;  }
-        };
-
-        std::vector<std::unique_ptr<MaskingRule>> all_masking_rules;
-
+        //options
     public:
-
-        SensitiveDataMasker() {}
-
-        void addMaskingRule(const std::string &name, const std::string &regexpString, const std::string &replacementString)
+        MaskingRule(const std::string & name, const std::string & regexpString, const std::string & replacementString)
+            : name(name)
+            , replacement_string(replacementString)
+            , regexp_string(regexpString)
+            , regexp(regexp_string, RE2::Quiet)
+            , replacement(replacement_string)
         {
-            all_masking_rules.push_back(std::make_unique<MaskingRule>(name, regexpString, replacementString));
+            if (!regexp.ok())
+                throw DB::Exception(
+                    "SensitiveDataMasker: cannot compile re2: " + regexpString
+                        + ", error: " + regexp.error()
+                        + ". Look at https://github.com/google/re2/wiki/Syntax for reference.",
+                    DB::ErrorCodes::CANNOT_COMPILE_REGEXP);
+        }
+        int apply(std::string & data)
+        {
+            auto m = RE2::GlobalReplace(&data, regexp, replacement);
+            std::lock_guard lock(mutex);
+            matches_count += m;
+            return m;
         }
 
-        int wipeSensitiveData(std::string &data)
-        {
-            int matches = 0;
-            for (auto &rule : all_masking_rules)
-            {
-                matches += rule->apply(data);
-            }
-            return matches;
-        }
-
-        void printStats()
-        {
-            for (auto &rule : all_masking_rules)
-            {
-                std::cout << rule->getName() << " (replacement to " << rule->getReplacementString() << ") matched " << rule->getMatchesCount() << " times" << std::endl;
-            }
-        }
+        const std::string & getName() const { return name; }
+        const std::string & getReplacementString() const { return replacement_string; }
+        uint64_t getMatchesCount() const { return matches_count; }
     };
+
+    std::vector<std::unique_ptr<MaskingRule>> all_masking_rules;
+
+public:
+    SensitiveDataMasker() {}
+
+    void addMaskingRule(const std::string & name, const std::string & regexpString, const std::string & replacementString)
+    {
+        all_masking_rules.push_back(std::make_unique<MaskingRule>(name, regexpString, replacementString));
+    }
+
+    int wipeSensitiveData(std::string & data)
+    {
+        int matches = 0;
+        for (auto & rule : all_masking_rules)
+        {
+            matches += rule->apply(data);
+        }
+        return matches;
+    }
+
+#ifndef NDEBUG
+    void printStats()
+    {
+        for (auto & rule : all_masking_rules)
+        {
+            std::cout << rule->getName() << " (replacement to " << rule->getReplacementString() << ") matched " << rule->getMatchesCount()
+                      << " times" << std::endl;
+        }
+    }
+#endif
+
+    unsigned long rulesCount() const { return all_masking_rules.size(); }
+};
 
 };
--- dbms/src/Common/tests/gtest_sensitive_data_masker.cpp
+++ dbms/src/Common/tests/gtest_sensitive_data_masker.cpp
@@ -1,10 +1,12 @@
+#include <Common/Exception.h>
 #include <Common/SensitiveDataMasker.h>
 
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #ifdef __clang__
-    #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
-    #pragma clang diagnostic ignored "-Wundef"
+#    pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#    pragma clang diagnostic ignored "-Wundef"
 #endif
+
 #include <gtest/gtest.h>
 
 
@@ -21,8 +23,68 @@ TEST(Common, SensitiveDataMasker)
     masker.addMaskingRule("rule \"d\" result", "--d--", "*****"); // RE2 regexps are applied one-by-one in order
     std::string x = "aaaaaaaaaaaaa   bbbbbbbbbb cccc aaaaaaaaaaaa d ";
     EXPECT_EQ(masker.wipeSensitiveData(x), 5);
-    masker.printStats();
     EXPECT_EQ(x, "--a--   --b-- cccc --a-- ***** ");
+#ifndef NDEBUG
+    masker.printStats();
+#endif
+    EXPECT_EQ(masker.wipeSensitiveData(x), 3);
+    EXPECT_EQ(x, "----a----   ----b---- cccc ----a---- ***** ");
+#ifndef NDEBUG
+    masker.printStats();
+#endif
 
-   // SensitiveDataMasker masker2;
+
+    SensitiveDataMasker masker2;
+    masker2.addMaskingRule("hide root password", "qwerty123", "******");
+    masker2.addMaskingRule("hide SSN", "[0-9]{3}-[0-9]{2}-[0-9]{4}", "000-00-0000");
+    masker2.addMaskingRule("hide email", "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", "hidden@hidden.test");
+
+    std::string query = "SELECT id FROM mysql('localhost:3308', 'database', 'table', 'root', 'qwerty123') WHERE ssn='123-45-6789' or "
+                        "email='JonhSmith@secret.domain.test'";
+    EXPECT_EQ(masker2.wipeSensitiveData(query), 3);
+    EXPECT_EQ(
+        query,
+        "SELECT id FROM mysql('localhost:3308', 'database', 'table', 'root', '******') WHERE "
+        "ssn='000-00-0000' or email='hidden@hidden.test'");
+
+
+#ifndef NDEBUG
+    // simple benchmark
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 200000; ++i)
+    {
+        std::string query2 = "SELECT id FROM mysql('localhost:3308', 'database', 'table', 'root', 'qwerty123') WHERE ssn='123-45-6789' or "
+                             "email='JonhSmith@secret.domain.test'";
+        masker2.wipeSensitiveData(query2);
+    }
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "Elapsed time: " << elapsed.count() << "s per 200000 calls (" << elapsed.count() * 1000000 / 200000 << "µs per call)"
+              << std::endl;
+    // I have: "Elapsed time: 3.44022s per 200000 calls (17.2011µs per call)"
+    masker2.printStats();
+#endif
+
+    SensitiveDataMasker maskerbad;
+
+    // gtest has not good way to check exception content, so just do it manually (see https://github.com/google/googletest/issues/952 )
+    try
+    {
+        maskerbad.addMaskingRule("bad regexp", "**", "");
+        ADD_FAILURE() << "addMaskingRule() should throw an error" << std::endl;
+    }
+    catch (DB::Exception & e)
+    {
+        EXPECT_EQ(
+            std::string(e.what()),
+            "SensitiveDataMasker: cannot compile re2: **, error: no argument for repetition operator: *. Look at "
+            "https://github.com/google/re2/wiki/Syntax for reference.");
+        EXPECT_EQ(e.code(), DB::ErrorCodes::CANNOT_COMPILE_REGEXP);
+    }
+    /* catch (...) { // not needed, gtest will react unhandled exception
+        FAIL() << "ERROR: Unexpected exception thrown: " << std::current_exception << std::endl; // std::current_exception is part of C++11x
+    } */
+
+    EXPECT_EQ(maskerbad.rulesCount(), 0);
+    EXPECT_EQ(maskerbad.wipeSensitiveData(x), 0);
 }
