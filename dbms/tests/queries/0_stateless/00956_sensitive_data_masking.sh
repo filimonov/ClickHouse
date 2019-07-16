a5325f6d13eee01365e4ce65b9ef5a914c637e58
Mikhail Filimonov <mfilimonov@altinity.com>
2019-07-16 14:24:23 +0200

    make test working

--- dbms/tests/queries/0_stateless/00956_sensitive_data_masking.sh
+++ dbms/tests/queries/0_stateless/00956_sensitive_data_masking.sh
@@ -11,36 +11,48 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cur_name=$(basename "${BASH_SOURCE[0]}")
 tmp_file=${CLICKHOUSE_TMP}/$cur_name"_server.logs"
 
+rm $tmp_file
+echo 1
 # normal execution
 $CLICKHOUSE_CLIENT \
   --query="SELECT 'find_me_TOPSECRET=TOPSECRET' FROM numbers(1) FORMAT Null" \
   --log_queries=1 --ignore-error --multiquery >$tmp_file 2>&1
 
-grep 'find_me_[hidden]' $tmp_file >/dev/null || echo 'fail 1a'
-grep 'TOPSECRET=TOPSECRET' $tmp_file && echo 'fail 1b'
+grep 'find_me_\[hidden\]' $tmp_file >/dev/null || echo 'fail 1a'
+grep 'TOPSECRET' $tmp_file && echo 'fail 1b'
 
+rm $tmp_file
+echo 2
 # failure at parsing stage
 echo "SELECT 'find_me_TOPSECRET=TOPSECRET' FRRRROM numbers" | ${CLICKHOUSE_CURL} -sSg ${CLICKHOUSE_URL} -d @- >$tmp_file 2>&1
 
-grep 'find_me_[hidden]' $tmp_file >/dev/null || echo 'fail 2a'
-grep 'TOPSECRET=TOPSECRET' $tmp_file && echo 'fail 2b'
+#cat $tmp_file
 
+## can't be checked on client side!
+# grep 'find_me_\[hidden\]' $tmp_file >/dev/null || echo 'fail 2a'
+grep 'TOPSECRET' $tmp_file && echo 'fail 2b'
+
+rm $tmp_file
+echo 3
 # failure at before query start
 $CLICKHOUSE_CLIENT \
   --query="SELECT 'find_me_TOPSECRET=TOPSECRET' FROM non_existing_table FORMAT Null" \
   --log_queries=1 --ignore-error --multiquery >$tmp_file 2>&1
 
-grep 'find_me_[hidden]' $tmp_file >/dev/null || echo 'fail 3a'
-grep 'TOPSECRET=TOPSECRET' $tmp_file && echo 'fail 3b'
+grep 'find_me_\[hidden\]' $tmp_file >/dev/null || echo 'fail 3a'
+grep 'TOPSECRET' $tmp_file && echo 'fail 3b'
 
+rm $tmp_file
+echo 4
 # failure at the end of query
 $CLICKHOUSE_CLIENT \
   --query="SELECT 'find_me_TOPSECRET=TOPSECRET', intDiv( 100, number - 10) FROM numbers(11) FORMAT Null" \
   --log_queries=1 --ignore-error --max_block_size=2 --multiquery >$tmp_file 2>&1
 
-grep 'find_me_[hidden]' $tmp_file >/dev/null || echo 'fail 4a'
-grep 'TOPSECRET=TOPSECRET' $tmp_file && echo 'fail 4b'
+grep 'find_me_\[hidden\]' $tmp_file >/dev/null || echo 'fail 4a'
+grep 'TOPSECRET' $tmp_file && echo 'fail 4b'
 
+echo 5
 # run in background
 bash -c "$CLICKHOUSE_CLIENT \
   --query=\"select sleepEachRow(0.5) from numbers(4) where ignore('find_me_TOPSECRET=TOPSECRET')=0 and ignore('fwerkh_that_magic_string_make_me_unique') = 0 FORMAT Null\" \
@@ -50,13 +62,13 @@ sleep 0.1
 
 # $CLICKHOUSE_CLIENT --query='SHOW PROCESSLIST'
 
+rm $tmp_file
+echo '5.1'
 # check that executing query doesn't expose secrets in processlist
-$CLICKHOUSE_CLIENT \
-  --query="SHOW PROCESSLIST" \
-  --log_queries=0 >$tmp_file 2>&1
+$CLICKHOUSE_CLIENT --query="SHOW PROCESSLIST" --log_queries=0 >$tmp_file 2>&1
 
 grep 'fwerkh_that_magic_string_make_me_unique' $tmp_file >/dev/null || echo 'fail 5a'
-grep 'fwerkh_that_magic_string_make_me_unique' | grep 'find_me_[hidden]' $tmp_file && echo 'fail 5b'
+( grep 'fwerkh_that_magic_string_make_me_unique' $tmp_file | grep 'find_me_\[hidden\]' $tmp_file >/dev/null ) || echo 'fail 5b'
 grep 'TOPSECRET' $tmp_file && echo 'fail 5c'
 
 wait
@@ -67,16 +79,22 @@ $CLICKHOUSE_CLIENT \
   --server_logs_file=/dev/null \
   --query="system flush logs"
 
+
+echo 6
 # check events count properly increments
 $CLICKHOUSE_CLIENT \
   --server_logs_file=/dev/null \
   --query="select * from (select sum(value) as matches from system.events where event='QueryMaskingRulesMatch') where matches < 5"
 
+echo 7
 # and finally querylog
 $CLICKHOUSE_CLIENT \
   --server_logs_file=/dev/null \
   --query="select * from system.query_log where event_time>now() - 10 and query like '%TOPSECRET%';"
 
+
+rm $tmp_file
+echo 8
 $CLICKHOUSE_CLIENT \
    --query="drop table if exists sensetive; create table sensitive ( id UInt64, date Date, value1 String, value2 UInt64) Engine=MergeTree ORDER BY id PARTITION BY date;
 insert into sensitive select number as id, toDate('2019-01-01') as date, 'abcd' as value1, rand() as valuer from numbers(10000);
@@ -85,5 +103,5 @@ insert into sensitive select number as id, toDate('2019-01-01') as date, 'abcd' 
 select * from sensitive WHERE value1 = 'find_me_TOPSECRET=TOPSECRET' FORMAT Null;
 drop table sensetive;" --log_queries=1 --ignore-error --multiquery >$tmp_file 2>&1
 
-grep 'find_me_[hidden]' $tmp_file >/dev/null || echo 'fail 4a'
-grep 'TOPSECRET' $tmp_file && echo 'fail 4b'
+grep 'find_me_\[hidden\]' $tmp_file >/dev/null || echo 'fail 8a'
+grep 'TOPSECRET' $tmp_file && echo 'fail 8b'
