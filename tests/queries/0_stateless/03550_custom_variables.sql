1c010bd562396986167c741ca17666f2b2720806
Mikhail Filimonov <mfilimonov@altinity.com>
2026-01-12 17:00:47 +0100

    basic test

---
+++ tests/queries/0_stateless/03550_custom_variables.sql
@@ -0,0 +1,20 @@
+-- Tags: no-parallel
+
+DROP VARIABLE IF EXISTS local.cv_test;
+
+CREATE VARIABLE local.cv_test AS toUInt32(1);
+SELECT getVariable('local.cv_test');
+
+SELECT name, scope, value, type, has_value, is_valid
+FROM system.custom_variables
+WHERE name = 'cv_test'
+ORDER BY name;
+
+CREATE OR REPLACE VARIABLE local.cv_test AS toUInt32(2);
+SELECT getVariable('local.cv_test');
+
+CREATE OR REPLACE VARIABLE local.cv_test AS 'x'; -- {serverError BAD_ARGUMENTS}
+CREATE VARIABLE local.cv_ref AS getVariable('local.cv_test'); -- {serverError BAD_ARGUMENTS}
+SELECT getVariable('local.missing'); -- {serverError BAD_ARGUMENTS}
+
+DROP VARIABLE local.cv_test;
